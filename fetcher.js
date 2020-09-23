d237b580f117d76d9f054017aa7adc69af1a72d9
amarkham1 <andrewjmarkham1@gmail.com>
2020-09-23 20:04:05 +0000

    implemented fetcher

--- fetcher.js
+++ fetcher.js
@@ -1,19 +1,46 @@
 const request = require('request');
 const fs = require('fs');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
 const fetcher = (URL, filePath) => {
   request(URL, (error, response, body) => {
     if (error) {
       throw new Error("Couldn't request from that URL!");
     }
-    fs.appendFile(filePath, body, error => {
-      if (error) {
-        throw new Error("Couldn't save to file!");
-      }
-      console.log("Saved to file!");
-    })
+    if (fileExists) {
+      rl.question("Are you sure you'd like to overwrite that file? (y/n) ", answer => {
+        if (answer === 'y') {
+          writeToFile(filePath, body);
+        } else if (answer === 'n') {
+          console.log("okay we won't overwrite the file");
+        } else {
+          console.log("please provide a (y/n) response");
+        }
+        rl.close();
+      });
+    } else {
+      writeToFile(filePath, body);
+    }
   });
 };
+
+const fileExists = filePath => {
+  return fs.access(filePath, fs.constants.F_OK, err);
+}
+
+const writeToFile = (filePath, data) => {
+  fs.writeFile(filePath, data, error => {
+    if (error) {
+      throw new Error("Couldn't save to file!");
+    }
+    console.log("Saved to file!");
+  })
+}
 
 if (process.argv.length < 4) {
   console.log("please provide a URL and filePath");
